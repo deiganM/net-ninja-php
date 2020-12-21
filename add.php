13157df57c7933b04b3b5c12bdbab3ed395f2434
Deigan Millikin <dmillikin@fieldsafesolutions.com>
2020-12-21 14:54:40 -0700

    Working with another tutorial add a database

--- add.php
+++ add.php
@@ -1,30 +1,47 @@
 <?php
-// if (isset($_GET['submit'])) {
-//     echo $_GET['email'];
-//     echo $_GET['title'];
-//     echo $_GET['ingredients'];
-// }
+
+$title = $email = $ingredients = '';
+
+$errors = [
+    'email' => '',
+    'title' => '',
+    'ingredients' => '',
+];
+
 if (isset($_POST['submit'])) {
     //check email
     if (empty($_POST['email'])) {
-        echo 'An email is required <br />';
+        $errors['email'] = 'An email is required <br />';
     } else {
         $email = $_POST['email'];
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          echo "Must be a valid email address"
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = "Must be a valid email address";
         }
     }
+
     //check title
     if (empty($_POST['title'])) {
-        echo 'A title is required <br />';
+        $errors['title'] = 'A title is required <br />';
     } else {
-        echo htmlspecialchars($_POST['title']);
+        $title = $_POST['title'];
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+            $errors['title'] = 'Title must be letters and spaces only';
+        }
     }
     //check ingredients
     if (empty($_POST['ingredients'])) {
-        echo 'Ingredients are required <br />';
+        $errors['ingredients'] = 'Ingredients are required <br />';
     } else {
-        echo htmlspecialchars($_POST['ingredients']);
+        $ingredients = $_POST['ingredients'];
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            $errors['ingredients'] = 'ingredients must be a comma separated list';
+        }
+    }
+    if (array_filter($errors)) {
+        // echo 'Errors in the form';
+    } else {
+        // echo 'Form is valid';
+        header('Location: index.php');
     }
 } //end of POST check
 ?>
@@ -37,12 +54,19 @@ if (isset($_POST['submit'])) {
 <container class="container grey-text">
   <h4 class="center">Add a Pizza</h4>
   <form action="add.php" method="POST" class="white">
+
     <label>Your Email:</label>
-    <input type="text" name="email">
+    <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+    <div class="red-text"><?=$errors['email'];?></div>
+
     <label>Pizza Title:</label>
-    <input type="text" name="title">
+    <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+    <div class="red-text"><?=$errors['title'];?></div>
+
     <label>Ingredients (comma separated):</label>
-    <input type="text" name="ingredients">
+    <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+    <div class="red-text"><?=$errors['ingredients'];?></div>
+
     <div class="center">
       <input type="submit" name="submit" class="btn brand z-depth-0">
     </div>
